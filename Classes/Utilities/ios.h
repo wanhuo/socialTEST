609506af8c1b67a1f1c7488c91b119862053fce0
Pythians <shuai_2009@126.com>
2014-09-22 16:03:36 +0800

    lose one

---
+++ Classes/Utilities/ios.h
@@ -0,0 +1,24 @@
+//
+//  ios.h
+//  ColorJump
+//
+//  Created by c++ on 14/9/22.
+//
+//
+
+#ifndef ColorJump_ios_h
+#define ColorJump_ios_h
+
+#include "IOSiAP_Bridge.h"
+
+
+#include "KTPlayC.h"
+#include "KTUserC.h"
+#include "KTErrorC.h"
+#include "KTFriendshipC.h"
+#include "KTLeaderboardC.h"
+#include "KTAccountManagerC.h"
+#include "KTLeaderboardPaginatorC.h"
+
+
+#endif
